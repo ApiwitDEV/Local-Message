4bcec58debec2a3ef9098a4763e60e5124b248a2
Apiwit Pakeesuk (Bas) <apiwit.pks@gmail.com>
2024-10-06 09:46:17 +0700

    clear focus

--- app/src/main/java/com/example/localmessage/MainActivity.kt
+++ app/src/main/java/com/example/localmessage/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,8 +20,10 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationRail
 import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import com.example.localmessage.ui.navigation.DestinationRoute
 import com.example.localmessage.ui.navigation.MainNavigation
@@ -33,10 +37,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             LocalMessageTheme {
                 val context = LocalContext.current
+                val focusManager = LocalFocusManager.current
                 val appUIStateHolder = rememberAppUIStateHolder(context)
                 when(appUIStateHolder.appOrientation) {
                     Configuration.ORIENTATION_PORTRAIT -> {
                         Scaffold(
+                            modifier = Modifier.clickable(
+                                interactionSource = remember { MutableInteractionSource() },
+                                indication = null
+                            ) { focusManager.clearFocus() },
                             bottomBar = {
                                 NavigationBar {
                                     NavigationBarItem(
@@ -68,7 +77,10 @@ class MainActivity : ComponentActivity() {
                         }
                     }
                     Configuration.ORIENTATION_LANDSCAPE -> {
-                        Row {
+                        Row(modifier = Modifier.clickable(
+                            interactionSource = remember { MutableInteractionSource() },
+                            indication = null
+                        ) { focusManager.clearFocus() }) {
                             NavigationRail {
                                 Column(
                                     modifier = Modifier.fillMaxHeight(),
